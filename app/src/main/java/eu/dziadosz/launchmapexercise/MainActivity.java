958a64f4ca4f7cac366cffd29bf9bd7d8e8d24f8
Radosław <radoslaw@dziadosz.eu>
2016-09-18 16:56:01 +0300

    Intent added

--- app/src/main/java/eu/dziadosz/launchmapexercise/MainActivity.java
+++ app/src/main/java/eu/dziadosz/launchmapexercise/MainActivity.java
@@ -1,7 +1,11 @@
 package eu.dziadosz.launchmapexercise;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showButtonClicked(View view){
+        // get lat and lng values
+        EditText latitudeEditText = (EditText) findViewById(R.id.latitude);
+        EditText longitudeEditText = (EditText) findViewById(R.id.longitude);
+        String numberOne = latitudeEditText.getText().toString();
+        String numberTwo = longitudeEditText.getText().toString();
+        double lat = Double.parseDouble(numberOne);
+        double lng = Double.parseDouble(numberTwo);
+        // show map
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:"+lat+","+lng));
+        startActivity(intent);
     }
 }
